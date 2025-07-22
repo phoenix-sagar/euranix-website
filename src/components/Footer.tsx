50d4fe8b62f74b18eb1fee3c603f4d7dbb3953e7
root <root@vmi2560527.contaboserver.net>
2025-07-22 14:35:32 +0530

    Added google verification html file and other updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -61,7 +61,7 @@ const Footer: React.FC = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 flex flex-col md:flex-row items-center justify-between">
           <p className="text-gray-400 text-sm">
-            © 2024 Euranix. All rights reserved.
+            © 2025 Euranix Technologies Pvt. Ltd. All rights reserved.
           </p>
           <div className="flex space-x-4 mt-4 md:mt-0">
             <a href="#" className="text-gray-400 hover:text-white transition-colors">
